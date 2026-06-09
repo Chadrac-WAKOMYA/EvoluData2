1ae37717c3a7a2a4c492eae693322b908c26018e
Chadrac-WAKOMYA <chadracxx@gmail.com>
2026-06-09 11:10:33 +0200

    fit sortedSearch finished

--- SortedSearch.java
+++ SortedSearch.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 public class SortedSearch {
     public static int countNumbers(int[] sortedArray, int lessThan) {
-        int i = 0;
-        for(){
-            
+        int index = Arrays.binarySearch(sortedArray, lessThan);
+
+        if (index < 0) {
+            // Si l'élément n'est pas trouvé, binarySearch retourne -(insertionPoint) - 1
+            return -index - 1;
+        } else {
+            // Si l'élément est trouvé, tous les éléments avant lui sont < lessThan
+            return index;
         }
-        return i;
     }
-    
+
     public static void main(String[] args) {
-        System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4));
+        System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4)); // Résultat attendu : 2
     }
 }
