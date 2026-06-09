a7c21d26ee746c4f0997ec98bdcf7de45f702ad9
Chadrac-WAKOMYA <chadracxx@gmail.com>
2026-06-09 10:46:48 +0200

    fit sortedSearch initialised

---
+++ SortedSearch.java
@@ -0,0 +1,13 @@
+public class SortedSearch {
+    public static int countNumbers(int[] sortedArray, int lessThan) {
+        int i = 0;
+        for(){
+            
+        }
+        return i;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+}
